c87d6e65a1d294fca75932dce123f71ad18441d7
Heitor Zelbato <heitorzelbato@gmail.com>
2025-04-04 17:12:26 -0300

    First Commits

---
+++ src/public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="../public/assets/css/output.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <h1 class="text-blue-400 text-4xl text-center p-40 ">Nutricionista
+        <span class="text-pink-300 text-4xl">Djhulia</span>.
+    </h1>
+
+</body>
+
+</html>
